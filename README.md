729792e23801d3eb2a3f0231de34263b6c78f539
corey arnold <84541944+arnoldc94@users.noreply.github.com>
2021-09-27 16:14:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Blog
+How to run this project:
+` $> python3 manage.py runserver`
+
+## Link to heroku URL
+https://desolate-waters-71087.herokuapp.com/ 
---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import BlogListView, BlogDetailView
+
+
+
+urlpatterns = [
+    path('', BlogListView.as_view(), name="home"),
+    path('blogs/<int:pk>/', BlogDetailView.as_view(), name='blog_detail')
+]
